b1e32f53370fbcc644a5ed6d7e3a10c029da7feb
Romain Lefranc <lefranc46@gmail.com>
2020-08-17 13:45:36 +0400

    v0.2

---
+++ Lab Bootstrap/listerBD.js
@@ -0,0 +1,63 @@
+var tableClient = new Array;
+tableClient[0] = new Client(1, 'Google', 'Omniscient', "2 999 000 000", '8745, Rue de l\'infini', 'Cercle', 'GAFA',"public", "0693907333");
+tableClient[1] = new Client(2, 'Apple',"Electronique","245 500 000", 'New-york City',"Pomme", "GAFA", "private", "0692123456");
+tableClient[2] = new Client(3, 'Microsoft',"Systeme Exploitation","9 999 999 998", 'LA',"Carré", "GAFA", "private", "0692123456");
+tableClient[3] = new Client(4, 'Samsung',"Téléphonie","10 210 000", 'Même rue que Huawai et LG',"SM", "GAFA", "private", "0692123456");
+tableClient[4] = new Client(5, 'Xiaomi',"Téléphonie","20 255 100", '25, Rue pavéD\'or de l\'infini',"Mi", "GAFA", "private", "0692123456");
+tableClient[5] = new Client(6, 'LG',"Téléphonie","250 000", 'A coté d\'un stands de bonbon près de SamsungCorps',"LG", "GAFA", "private", "0692123456");
+tableClient[6] = new Client(7, 'Amazone',"Vente en tout genre","5 000 000 ", 'Inde',"Rond", "GAFA", "private", "0692123456");
+tableClient[7] = new Client(8, 'Sony',"Jeux Vidéo","155 500 000", 'Chine',"Sony", "GAFA", "private", "0692123456");
+tableClient[8] = new Client(9, 'XboxCorp',"Jeux Vidéo","100 000", 'A coté de Microsoft',"X", "GAFA", "private", "0692123456");
+tableClient[9] = new Client(10, 'Huawei',"Téléphonie","800 000", 'A coté de Sony',"Fleur", "GAFA", "private", "0692123456");
+tableClient[10] = new Client(11, 'Huawei',"Téléphonie","800 000", 'A coté de Sony',"Fleur", "GAFA", "private", "0692123456");
+tableClient[11] = new Client(12, 'Huawei',"Téléphonie","800 000", 'A coté de Sony',"Fleur", "GAFA", "private", "0692123456");
+tableClient[12] = new Client(13, 'Huawei',"Téléphonie","800 000", 'A coté de Sony',"Fleur", "GAFA", "private", "0692123456");
+tableClient[13] = new Client(14, 'Huawei',"Téléphonie","800 000", 'A coté de Sony',"Fleur", "GAFA", "private", "0692123456");
+tableClient[14] = new Client(15, 'Huawei',"Téléphonie","800 000", 'A coté de Sony',"Fleur", "GAFA", "private", "0692123456");
+tableClient[15] = new Client(16, 'Huawei',"Téléphonie","800 000", 'A coté de Sony',"Fleur", "GAFA", "private", "0692123456");
+tableClient[16] = new Client(17, 'Huawei',"Téléphonie","800 000", 'A coté de Sony',"Fleur", "GAFA", "private", "0692123456");
+tableClient[17] = new Client(18, 'Huawei',"Téléphonie","800 000", 'A coté de Sony',"Fleur", "GAFA", "private", "0692123456");
+tableClient[18] = new Client(19, 'Huawei',"Téléphonie","800 000", 'A coté de Sony',"Fleur", "GAFA", "private", "0692123456");
+tableClient[19] = new Client(20, 'Huawei',"Téléphonie","800 000", 'A coté de Sony',"Fleur", "GAFA", "private", "0692123456");
+tableClient[20] = new Client(21, 'Huawei',"Téléphonie","800 000", 'A coté de Sony',"Fleur", "GAFA", "private", "0692123456");
+tableClient[21] = new Client(22, 'Huawei',"Téléphonie","800 000", 'A coté de Sony',"Fleur", "GAFA", "private", "0692123456");
+
+/**
+ * M : Inserer dans un tableau html , un tableau JS
+ * O : /
+ * I : @param {*} contact le tableau JS
+ */
+function afficherliste(client) {
+  // Récupération de l'élement
+  const elApp = document.getElementsByClassName("tableClient")[0];
+  elApp.innerHTML = "";
+
+  let data = '<div class="row">';
+  // Récupération des données
+  client.forEach(c => {
+    data += 
+    `<div class="col-2">
+        <div class="imageLivre">
+        </div>
+        <p>${c.getNumClient()}</p>
+    </div>`;
+  });
+
+  if (data.length > 0) {
+    // Affichage des données dans le tableau
+    elApp.innerHTML += data;
+  } else {
+    // Aucune donnée
+    elApp.innerHTML += "Aucune ligne trouvée";
+  }
+}
+// affiche le tableau de client
+afficherliste(tableClient);
+
+// rechercher un client par nom de societe
+  document.querySelectorAll("input[type=search]")[0].addEventListener("input", function() {
+    const filteredData = tableClient.filter(client =>
+        client.getNomSociete().toLowerCase().includes(this.value.toLowerCase())
+      );
+    afficherliste(filteredData);
+  });
