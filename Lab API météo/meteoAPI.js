6b0f09998bd79d4909d5e25bf7391530f7383608
DEVLOG.E09 <DEVLOG.E09@afpar.com>
2020-08-21 08:55:37 +0400

    v1.4

--- Lab API météo/meteoAPI.js
+++ Lab API météo/meteoAPI.js
@@ -20,19 +20,19 @@ $( document ).ready(function() {
     }
     $("#button").click(function() {
             url = "https://api.openweathermap.org/data/2.5/weather?lat="+ latitude + "&lon="+ longitude + "&appid=65372f0a54dd75934920a83319e364a6&lang=fr&units=metric"
-            /* $.get(url, function(data) {
-                x.html("La temperature de " + data.name + " est de "+ data.main.feels_like +"°C, c'est un temp " + data.weather[0].description)
+            $.get(url, function(data) {
+                x.html("La temperature de " + data.name + " est de "+ data.main.temp +"°C, c'est un temp " + data.weather[0].description)
                 var imgUrl = "http://openweathermap.org/img/wn/"+ data.weather[0].icon+"@2x.png"
                 $( "#img").attr("src", imgUrl)
             }).fail(function() {
                 x.html("Méteo indisponible pour le moment")
-            }) */
+            })
             $.ajax({
                 url : url,
                 type : "GET",
                 dataType : "json",
                 success : function (data) {
-                    x.html("La temperature de " + data.name + " est de "+ data.main.feels_like +"°C, c'est un temp " + data.weather[0].description)
+                    x.html("La temperature de " + data.name + " est de "+ data.main.temp +"°C, c'est un temp " + data.weather[0].description)
                     var imgUrl = "http://openweathermap.org/img/wn/"+ data.weather[0].icon+"@2x.png"
                     $( "#img").attr("src", imgUrl)
                 },
