ee7c47b078c76b6f023d55588ab3cb3c2c0f6475
RomainLefranc <lefranc46@gmail.com>
2020-08-21 21:02:37 +0400

    v1.8

--- Lab API météo/meteoAPI.js
+++ Lab API météo/meteoAPI.js
@@ -1,28 +1,27 @@
 /*
 https://api.openweathermap.org/data/2.5/weather?lat=-20.9628&lon=55.6501&appid=65372f0a54dd75934920a83319e364a6&lang=fr&units=metric  Saint-André
 */
-$( document ).ready(function() {
-    var latitude;
-    var longitude;
-    var url;
-    var x = $("#demo");
+var latitude;
+var longitude;
+var x = $("#demo");
+if (navigator.geolocation) {
+    /* function showPosition(position) {
+        latitude = position.coords.latitude
+        longitude = position.coords.longitude
+    } */
+    navigator.geolocation.getCurrentPosition(function (position) {
+        latitude = position.coords.latitude
+        longitude = position.coords.longitude
+    });
+    $("#button").show()
+} else {
+    x.html("Geolocalisation désactivé")
     $("#button").hide();
-    if (navigator.geolocation) {
-        /* function showPosition(position) {
-            latitude = position.coords.latitude
-            longitude = position.coords.longitude
-        } */
-        navigator.geolocation.getCurrentPosition(function (position) {
-            latitude = position.coords.latitude
-            longitude = position.coords.longitude
-        });
-        $("#button").show()
-    } else {
-        x.html("Geolocalisation désactivé")
-        
-    }
+    
+}
+$( document ).ready(function() {
+    var url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=65372f0a54dd75934920a83319e364a6&lang=fr&units=metric`
     $("#button").click(function() {
-            url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=65372f0a54dd75934920a83319e364a6&lang=fr&units=metric`
             $.get(url, function(data) {
                 console.log(data)
                 x.html(`La temperature de ${data.name} est de ${data.main.temp}°C, c'est un temp ${data.weather[0].description}`)
