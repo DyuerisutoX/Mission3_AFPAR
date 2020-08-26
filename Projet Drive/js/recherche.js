d7c7d7a83cc1e2e405b7f99f6dfeba2174279df0
DEVLOG.E04 <DEVLOG.E04@afpar.com>
2020-08-26 09:27:40 +0400

    v2.1 code recherche BD

--- Projet Drive/js/recherche.js
+++ Projet Drive/js/recherche.js
@@ -1,3 +1,37 @@
-// recherche
-var recherche = "Le sens de la vie"
-var albumsRecherche = new Map([...series].filter(([cle, valeur]) => valeur.nom.includes(recherche)))
+$(document).ready(function (){
+
+
+            $('.navbar-form').keyup(function (event) {
+                let keycode = (event.keyCode ? event.keyCode : event.wchich);
+                // recherche
+                var recherche = document.getElementById('rechercheNav').value;                                              //Récupère valeur de la barre de recherche
+                var albumsRecherche = new Map([...albums].filter(([cle, valeur]) => valeur.titre.includes(recherche)));     
+                var lecture = "Marsupilami";
+            
+                
+                // function prbImg(element) {
+                //   // console.log(element);
+                //   if (element.id === "albumMini")
+                //     element.src = albumDefaultMini;
+                //   else element.src = albumDefault;
+                // }
+            
+                if(keycode == '13'){
+
+                        //console.log(albumsRecherche); 
+                        console.log(albumsRecherche);
+                    
+                }
+                else
+                {
+                    event.preventDefault();
+                    //console.log('Test KO');
+                }
+            });
+            
+
+
+
+
+});
+
