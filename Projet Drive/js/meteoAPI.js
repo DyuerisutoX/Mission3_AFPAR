291f19e519ddb0b310193ea6e071ce3aead0f913
RomainLefranc <lefranc46@gmail.com>
2020-08-30 11:43:52 +0400

    v2.0

--- Projet Drive/js/meteoAPI.js
+++ Projet Drive/js/meteoAPI.js
@@ -1,46 +1,45 @@
 /* https://api.openweathermap.org/data/2.5/weather?lat=-20.9628&lon=55.6501&appid=65372f0a54dd75934920a83319e364a6&lang=fr&units=metric  Saint-André */
-var latitude;
-var longitude;
-if (navigator.geolocation) {
-    /* 
-    M : recupere la longitude et le latitude de la position de l'utilisateur
-    O : /
-    I : position
-    */
-    navigator.geolocation.getCurrentPosition(function (position) {
-        latitude = position.coords.latitude
-        longitude = position.coords.longitude
-    });
-} else {
-    $("#meteo").html("Geolocalisation désactivé")   
-}
+
 /* 
 M : Execute la fonction au moment ou le document est pret
 O : /
 I : /
 */
 $( document ).ready(function() {
-    if (latitude !== undefined) {
-        var url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=65372f0a54dd75934920a83319e364a6&lang=fr&units=metric`
-        $.get(url, function(data) {
-            $("#meteo").html(`La température de ${data.name} est de ${data.main.temp}°C, c'est un temp ${data.weather[0].description}`)
-            var imgUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-            $( "#imgMeteo").attr("src", imgUrl)
-        }).fail(function() {
-            $("#meteo").html("Méteo indisponible pour le moment")
+    if (navigator.geolocation) {
+        /* 
+        M : recupere la longitude et le latitude de la position de l'utilisateur
+        O : /
+        I : position
+        */
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var latitude = position.coords.latitude
+            var longitude = position.coords.longitude
+            var url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=65372f0a54dd75934920a83319e364a6&lang=fr&units=metric`
+            $.get(url, function(data) {
+                $("#meteo").html(`La température de ${data.name} est de ${data.main.temp}°C, c'est un temp ${data.weather[0].description}`)
+                var imgUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+                $( "#imgMeteo").attr("src", imgUrl)
+            }).fail(function() {
+                $("#meteo").html("Méteo indisponible pour le moment")
+            });
+/*             $.ajax({
+                url : url,
+                type : "GET",
+                dataType : "json",
+                success : function (data) {
+                    $("#meteo").html(`La temperature de ${data.name} est de ${data.main.temp}°C, c'est un temp ${data.weather[0].description}`)
+                    var imgUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` 
+                    $( "#img").attr("src", imgUrl)
+                },
+                error : function () {
+                    $("#meteo").html("Méteo indisponible pour le moment")
+                }
+            }) */
         })
+    } else {
+        $("#meteo").html("Geolocalisation désactivé")   
     }
-/*     $.ajax({
-        url : url,
-        type : "GET",
-        dataType : "json",
-        success : function (data) {
-            $("#meteo").html(`La temperature de ${data.name} est de ${data.main.temp}°C, c'est un temp ${data.weather[0].description}`)
-            var imgUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-            $( "#img").attr("src", imgUrl)
-        },
-        error : function () {
-            $("#meteo").html("Méteo indisponible pour le moment")
-        }
-    }) */
 })
+
+
